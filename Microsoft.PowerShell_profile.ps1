07c454c749f8db83937a817873f2a2a87783fa45
Lars <lars.ro@36-s.com>
2015-06-17 10:20:23 +0200

    Nicer messages in profile startup

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -19,8 +19,6 @@ function Open-Solution
   $solution = gci *.sln | Select-Object -First 1
   devenv $solution
 }
-New-Alias msdev Open-Solution
-New-Alias  -name SourceTree "C:\Program Files (x86)\Atlassian\SourceTree\SourceTree.exe" -Scope "global"
 function Goto-TestExecution
 {
   Push-Location C:\Depot\Infrastructure\source\Core\
@@ -34,12 +32,27 @@ function Goto-Powershell
   Push-Location $PROFILE\..
 }
 
+Write-host "Starting PowerShell Profile for " -NoNewLine
+Write-host "$env:USERNAME" -ForegroundColor Green
+
+Write-host "Configuring Paths and Aliases..." -NoNewLine -ForegroundColor Yellow
+
+New-Alias msdev Open-Solution
+New-Alias  -name SourceTree "C:\Program Files (x86)\Atlassian\SourceTree\SourceTree.exe" -Scope "global"
+
 $Env:Path+=";$Env:ProgramData\chocolatey\lib\sysinternals\tools\"
 
+Write-host "Done!" -ForegroundColor Green
+
+Write-host "Starting VS CMD Environment..." -NoNewLine -ForegroundColor Yellow
 Invoke-Environment (join-path $env:VS120COMNTOOLS "vsvars32.bat")
+Write-host "Done!" -ForegroundColor Green
+
+Write-host "Starting posh-git..." -NoNewLine -ForegroundColor Yellow
 # Load posh-git example profile
 if(Test-Path Function:\Prompt) {Rename-Item Function:\Prompt PrePoshGitPrompt -Force}
 . 'C:\tools\poshgit\dahlbyk-posh-git-7acc70b\profile.example.ps1'
 Rename-Item Function:\Prompt PoshGitPrompt -Force
 function Prompt() {if(Test-Path Function:\PrePoshGitPrompt){++$global:poshScope; New-Item function:\script:Write-host -value "param([object] `$object, `$backgroundColor, `$foregroundColor, [switch] `$nonewline) " -Force | Out-Null;$private:p = PrePoshGitPrompt; if(--$global:poshScope -eq 0) {Remove-Item function:\Write-Host -Force}}PoshGitPrompt}
+Write-host "Done!" -ForegroundColor Green
 cd $env:USERPROFILE
